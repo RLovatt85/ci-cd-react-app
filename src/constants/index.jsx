83d8faf24dee91f941020121330325797e6d077f
RLovatt85 <64983399+RLovatt85@users.noreply.github.com>
2026-08-18 15:26:37 -0400

    Update yearly price for Overdrive plan

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -129,7 +129,7 @@ export const features = [
       id: "1",
       title: "Overdrive",
       priceMonthly: 79,
-      priceYearly: 68,
+      priceYearly: 95,
       caption: "Most popular plan",
       features: [
         "All Starter features",
